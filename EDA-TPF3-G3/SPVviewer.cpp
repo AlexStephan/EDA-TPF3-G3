569e5a874dcc291aedea1c64acffed814974b93b
ALE <aleronpower@hotmail.com>
2019-11-09 20:57:26 -0300

    Instrucciones para SPVviewer (OLI)
    En resumen. OLI esta en condiciones de hacer:
    -Controller y viewer de EDAcoinsNet
    -Controller y viewer de SPVNode
    -Controller de FULLNode
    
    Advertencia: las cosas pueden complicarse un poco cuando tengamos q hacer el viewer de FULLNode, asi q a meterle pata a esta parte, q no es tan dificil. Cuento contigo
    
    Nah, al cabo q nadie se molesta en leer los commits enteros

--- EDA-TPF3-G3/SPVviewer.cpp
+++ EDA-TPF3-G3/SPVviewer.cpp
@@ -1,1 +1,20 @@
 #include "SPVviewer.h"
+
+#define NO_DATA	"NO DATA",0,0,0,0,0
+
+SPVviewer::SPVviewer() :
+	nodedata(NO_DATA), headernodedata(NO_DATA),
+	filternodedata(NO_DATA), windowName("NO_DATA")
+{
+}
+
+void SPVviewer::update(void* n)
+{
+	SPVNode* node = (SPVNode*)n;
+
+	nodedata = node->getData();
+	headernodedata = node->getHeaderNodeData();
+	filternodedata = node->getFilterNodeData();
+
+	windowName = nodedata.getID();
+}
--- EDA-TPF3-G3/SPVviewer.h
+++ EDA-TPF3-G3/SPVviewer.h
@@ -7,7 +7,20 @@ class SPVviewer :
 	public Observer
 {
 public:
+	SPVviewer();
 	virtual void update(void*);
-	void cycle();
+	void cycle(); //OLI
+	//Dibuja: datos del nodo, y datos de los dos nodos FULL vecinos
+	//(especificando cual es cual)
+	//puede usar los campos q ya estan definidos (se actualizan en el update)
+	//asi q no es necesario llamar a metodos de SPV... creo
+	//Recordar: nombre de la ventana: windowName
+
+
+private:
+	NodeData nodedata;
+	string windowName;
+	NodeData headernodedata;
+	NodeData filternodedata;
 };
 
