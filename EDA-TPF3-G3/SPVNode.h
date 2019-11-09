da6b7d70e2b3560cfb82556f464b932afdecf91e
ALE <aleronpower@hotmail.com>
2019-11-09 20:14:47 -0300

    declaracion de SPVnode practicamente terminada (osea, lo q quiero q disponga)

--- EDA-TPF3-G3/SPVNode.h
+++ EDA-TPF3-G3/SPVNode.h
@@ -8,6 +8,18 @@ public:
 	SPVNode(NodeData ownData, NodeData FilterNode, NodeData HeaderNode) :
 		Node(ownData), filterNode(FilterNode), headerNode(HeaderNode) {}
 	virtual void cycle();
+
+	//Metodos accedidos desde el controller
+	errorType makeTX(const vector<Vout>& receivers);
+	errorType postFilter();
+	errorType changeFilterNode(NodeData FilterNode);
+	errorType changeHeaderNode(NodeData HeaderNode);
+
+	//Accedidas desde el viewer:
+	NodeData getData();
+	NodeData getFilterNodeData();
+	NodeData getHeaderNodeData();
+
 private:
 	NodeData filterNode;
 	NodeData headerNode;
