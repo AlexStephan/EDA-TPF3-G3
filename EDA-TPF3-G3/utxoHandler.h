05a3e26b59e8ef8155a21d335f08665f7931ffae
ALE <aleronpower@hotmail.com>
2019-11-26 20:05:03 -0300

    ah, si, declaraciones

--- EDA-TPF3-G3/utxoHandler.h
+++ EDA-TPF3-G3/utxoHandler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "utxo.h"
+#include "errorType.h"
 
 class utxoHandler
 {
@@ -9,13 +10,20 @@ public:
 	void initializeUtxo(); //ASUMO Q LA BLOCKCHAIN ES VALIDA
 	longN balance(string publicKey);
 
+	bool TxExistAlready(Transaction& tx);
+	errorType validateTX(Transaction& tx); //ANTES COMPROBAR HASH Y FIRMA
+	void insertTX(Transaction& tx);
 
+	bool BLockExistAlready(Block& block);
+	errorType validateBlock(Block& block); //ANTES COMPROBAR HASH, CHALLENGE Y PREVIOUS_ID
+	void insertBlock(Block& block);
 
 private:
 	vector<utxo> utxoList;
 
 	BlockChain* blockChain;
 	vector<Transaction>* txs;
+
 
 	bool findUtxo(const string& id, size_t indexInList);
 		
